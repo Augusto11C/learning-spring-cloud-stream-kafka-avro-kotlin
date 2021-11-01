a305ab540b03470d9840eba4fc94bfaef4a31545
Augusto Calado Bueno <acalado@uolinc.com>
2021-11-01 18:08:16 -0300

    attempts to infinit retry kafka

--- src/main/kotlin/aug/bueno/consumer/NotificationConsumerDLQ.kt
+++ src/main/kotlin/aug/bueno/consumer/NotificationConsumerDLQ.kt
@@ -2,19 +2,19 @@ package aug.bueno.consumer
 
 import aug.bueno.entity.Notification
 import mu.KLogger
-import mu.KotlinLogging.logger
+import mu.KotlinLogging
 import org.springframework.stereotype.Component
 import java.util.function.Consumer
 
-@Component("notificationConsumer")
-class NotificationConsumer(
-    private val logger: KLogger = logger {},
+//@Component("notificationConsumerDLQ")
+class NotificationConsumerDLQ(
+    private val logger: KLogger = KotlinLogging.logger {},
 ) : Consumer<Notification> {
 
     override fun accept(event: Notification) {
-        this.logger.info { "m=accept, event=$event" }
+        this.logger.info { "m=accept, Handling ERROR in the event=$event" }
 
-        if(event.msg == "ERROR"){
+        if (event.msg == "ERROR") {
             throw RuntimeException("problem msg with ERROR")
         }
     }
--- src/main/kotlin/aug/bueno/producer/NotificationProducer.kt
+++ src/main/kotlin/aug/bueno/producer/NotificationProducer.kt
@@ -1,14 +1,18 @@
 package aug.bueno.producer
 
 import aug.bueno.entity.Notification
+import mu.KLogger
+import mu.KotlinLogging
 import org.springframework.cloud.stream.function.StreamBridge
 import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Component
 
 @Component("notificationProducer")
-class NotificationProducer(private val bridge: StreamBridge) {
+class NotificationProducer(private val logger: KLogger = KotlinLogging.logger {},
+                           private val bridge: StreamBridge) {
 
     infix fun send(event: Notification) {
+            this.logger.info { "m=send, event=$event" }
         val message = MessageBuilder.withPayload(event).build()
         this.bridge.send("notification-topic-v1", message)
     }
